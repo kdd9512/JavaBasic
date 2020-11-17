48efe7c05b44ce6c6d28019f512ed8f60c4613db
kdd9512 <hyunwoo8805@gmail.com>
2020-11-17 12:34:30 +0900

    SungJukV2 스캐너 + switch문 이용한 학점처리 나의 답안.

---
+++ src/kdd9512/SungJukV2.java
@@ -0,0 +1,65 @@
+package kdd9512;
+
+import java.util.Scanner;
+
+/**
+/* 파일명 : SungJukV1
+ * 작성일 : 2020.11.17
+ *
+ * 프로그램설명 : 성적처리 프로그램 v1.
+ * 이름, 국어, 영어, 수학점수를 설정하면,
+ * 총점, 평균, 학점을 계산, 결과를 출력.
+ *
+ * 100점 만점. 100~90, 89~80, 79~70, 69~60, 59~0 기준으로 학점 부여.
+ * 학점기준 : 수,우,미,양,가.
+ * switch문으로 학점을 계산하도록 함.
+ * name, kor, eng, mat, sum, mean, grd.
+ * 이름, 국어, 영어, 수학, 총점, 평균, 등급.
+ *
+ *
+ */
+public class SungJukV2 {
+    public static void main(String[] args) {
+        String name = "";
+        int kor, eng, mat;
+        int sum = 0;
+        double mean = 0.0;
+        char grd = ' ';
+        //변수선언
+        Scanner scan = new Scanner(System.in);
+        System.out.println("이름");
+        name = scan.nextLine();
+        System.out.println("국어성적");
+        kor = scan.nextInt();
+        System.out.println("수학성적");
+        mat = scan.nextInt();
+        System.out.println("영어성적");
+        eng = scan.nextInt();
+
+        sum = kor+mat+eng;
+        mean = (double)sum/3;
+        grd = ' ';
+
+        switch((int)(mean/10)){
+            case 9 : grd = '수';
+                break;
+            case 8 : grd = '우';
+                break;
+            case 7 : grd = '미';
+                break;
+            case 6 : grd = '양';
+                break;
+            default : grd = '가';
+                break;
+        }
+        System.out.println("이름 = " + name);
+        System.out.println("국어 = " + kor);
+        System.out.println("수학 = " + mat);
+        System.out.println("영어 = " + eng);
+        System.out.println("-------------");
+        System.out.println("총점 = " + sum);
+        System.out.printf("\n평균 = %.2f\n ",mean);
+        System.out.println("학점 = " + grd);
+
+    }
+}
